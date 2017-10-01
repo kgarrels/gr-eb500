fa56f00a5df3dce1c38b69ac3d9380dc773f1086
Kai Garrels <kai.garrels@gmx.de>
2017-10-01 19:56:28 +0200

    sample rate now works as a parameter with callback

--- python/control.py
+++ python/control.py
@@ -26,7 +26,11 @@ import telnetlib
 
 
 
-
+# TODO
+# - expose cmd and udp ports as parameters
+# - paramter for additional stream tags?
+# - put borip_server.py and borip-EB500 stuff into examples
+# - add message ports - which protocol?
 
 
 class EB500Cmd (telnetlib.Telnet):
@@ -52,21 +56,40 @@ class EB500Control(gr.basic_block):
     CMD_PORT = 5555
     UDP_PORT = 28888
     
-    def __init__(self, center_freq=14200000, samp_rate=640000, address = EB500_ADDR, port = CMD_PORT ):
+    def __init__(self, center_freq=14200000, sample_rate=640000, address = EB500_ADDR, port = CMD_PORT ):
         gr.basic_block.__init__(self, name="control", in_sig=None, out_sig=None)
         self.center_freq = center_freq
-        self.samp_rate = samp_rate
+        self.sample_rate = sample_rate
         self.eb500d = EB500Cmd(address, port)
         
         self.eb500d.send_cmd('TRAC:UDP:DEL ALL')
-        self.eb500d.send_cmd('TRAC:UDP:TAG \"192.168.1.32\", ' + str(19000) + ',IF')
+        self.set_center_freq(center_freq)
+        self.set_sample_rate(sample_rate)
         self.eb500d.send_cmd('SYST:IF:REM:MODE SHORT')
         self.eb500d.send_cmd('SYST:COMM:LAN:PING 0')
-
+        self.eb500d.send_cmd('TRAC:UDP:TAG \"192.168.1.32\", ' + str(19000) + ',IF')
+        
     def set_center_freq(self, freq):
         cmd = "freq " + str(freq)
         self.eb500d.send_cmd(cmd)
 
+    def sampRate_to_bandWidth(self, samp):
+        switcher = {
+            320000: 250000,
+            640000: 500000,
+            1280000: 1000000,
+            3200000: 2000000,
+            6400000: 5000000
+        }
+        return switcher.get(samp)
+
+    def set_sample_rate(self, sample_rate):
+        bandwidth = self.sampRate_to_bandWidth(sample_rate)
+        cmd = "band " + str(bandwidth)
+        print "set bw " + str(bandwidth) + " from sr " + str(sample_rate)
+        self.eb500d.send_cmd('TRAC:UDP:DEL ALL')
+        self.eb500d.send_cmd(cmd)
+        self.eb500d.send_cmd('TRAC:UDP:TAG \"192.168.1.32\", ' + str(19000) + ',IF')
 
 
     def general_work(self) :    #, input_items, output_items):
